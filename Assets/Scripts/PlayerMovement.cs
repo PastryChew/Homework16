2001269c2881b96a32438ac51ec81addd8d7def0
PastryChew <80216822+PastryChew@users.noreply.github.com>
2022-02-01 14:54:44 +0300

    18

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+
+    [Header("Movement vars")]
     [SerializeField] private float JumpForce;
+    [SerializeField] private float speed;
+    [SerializeField] private bool isGrounded = false;
+
+    [Header("Settings")]
+    [SerializeField] private float JumpOffset;
+    [SerializeField] private Transform GroundColliderTransform;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private AnimationCurve curve;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate()
     {
-        
+        Vector3 overlapCirclePosition = GroundColliderTransform.position;
+       isGrounded = Physics2D.OverlapCircle(overlapCirclePosition, JumpOffset, groundMask);
     }
     public void Move(float Direction, bool isJumpButtonPressed)
     {
@@ -21,10 +33,22 @@ public class PlayerMovement : MonoBehaviour
         {
             Jump();
         }
+
+        if (Mathf.Abs(Direction) > 0.01f)
+        {
+            HorizontalMovement(Direction);
+        }
     }
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+        }
+    }
+    private void HorizontalMovement(float Direction)
+    {
+        rb.velocity = new Vector2(curve.Evaluate(Direction), rb.velocity.y);
     }
 }
